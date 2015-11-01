50860a37ce1dcbefaafd0fae9ad3f474f6636e5b
AoEiuV020 <490674483@qq.com>
2015-11-02 00:54:36 +0800

    08

---
+++ linux/08/index.php
@@ -0,0 +1,9 @@
+<!--***************************************************
+	^> File Name: index.php
+	^> Author: AoEiuV020
+	^> Mail: 490674483@qq.com
+	^> Created Time: 2015/10/15 - 19:25:44
+***************************************************-->
+<?php
+include $_SERVER["DOCUMENT_ROOT"]."/index.php";
+?>
